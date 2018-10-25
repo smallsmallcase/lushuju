571ca1e1180990f94a729db3cbdaa6ca0dc8d45d
smallsmallcase <1150710274@qq.com>
2018-10-25 18:54:17 +0800

    跨域

--- src/main/java/com/smallcase/lushuju/CrosFilter.java
+++ src/main/java/com/smallcase/lushuju/CrosFilter.java
@@ -1,41 +1,71 @@
 package com.smallcase.lushuju;
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.shiro.web.filter.authc.UserFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 
 /**
  * Package: com.smallcase.lushuju
  * Author: smallcase
  * Date: Created in 2018/8/9 21:23
  */
-public class CrosFilter implements Filter{
 
+/**
+ * 重写shiro的UserFilter，实现通过OPTIONS请求
+ * @author MDY
+ */
+public class CrosFilter extends UserFilter {
+
+    /**
+     * 在访问过来的时候检测是否为OPTIONS请求，如果是就直接返回true
+     */
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
+    protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        if (httpRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            setHeader(httpRequest,httpResponse);
+            return true;
+        }
+        return super.preHandle(request,response);
     }
 
+    /**
+     * 该方法会在验证失败后调用，这里由于是前后端分离，后台不控制页面跳转
+     * 因此重写改成传输JSON数据
+     */
     @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) resp;
-        String origin = request.getHeader("Origin");
-        String header = request.getHeader("Access-Control-Request-Headers");
-//        String contentType = request.getHeader("Content-Type");
-
-//        response.addHeader("Access-Control-Allow-Headers", contentType);
-        response.addHeader("Access-Control-Allow-Headers", header);
-        response.addHeader("Access-Control-Allow-origin", origin);
-        response.addHeader("Access-Control-Allow-Methods", "*");
-        response.addHeader("Access-Control-Allow-Max-Age", "3600");
-
-        chain.doFilter(req, response);
+    protected void saveRequestAndRedirectToLogin(ServletRequest request, ServletResponse response) throws IOException {
+        saveRequest(request);
+        setHeader((HttpServletRequest) request,(HttpServletResponse) response);
+        PrintWriter out = response.getWriter();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("失败", "失败");
+        out.println(JSONObject.toJSONString(map));
+        out.flush();
+        out.close();
     }
 
-    @Override
-    public void destroy() {
-
+    /**
+     * 为response设置header，实现跨域
+     */
+    private void setHeader(HttpServletRequest request,HttpServletResponse response){
+        //跨域的header设置
+        response.setHeader("Access-control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Methods", request.getMethod());
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+        //防止乱码，适用于传输JSON数据
+        response.setHeader("Content-Type","application/json;charset=UTF-8");
+        response.setStatus(HttpStatus.OK.value());
     }
+
 }
